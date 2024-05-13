7d822743eead3833c069c668640507e2e013fc49
dhairyapatel1506 <ts554408@gmail.com>
2024-05-13 16:50:27 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from langchain import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Pinecone
-import pinecone
-from langchain.document_loaders import PyMuPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import chainlit as cl
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
-from langchain.llms import ctransformers
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hiiii, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+os.environ["NVIDIA_API_KEY"] = "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"
+os.environ["CHAINLIT_AUTH_SECRET"] = "Pm3sLU>rs+h$d7yRoCwCERUesF@Jc0TJnkgb=-ljbSeyf9AEcRdmEIR1sdJef9gN"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def bot():
+    llm = ChatNVIDIA(model="mixtral_8x7b")
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    medibot_template = """You are a helpful AI medical assistant named MediBot. You will only reply to medical 
+    queries. If something is out of context, you will refrain from replying and politely decline to respond to the 
+    user.
+    Chat History: {chat_history} Input: {input} Answer:"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    prompt_template = PromptTemplate(
+        input_variables=["chat_history", "input"],
+        template=medibot_template
+    )
+
+    chain = LLMChain(prompt=prompt_template, llm=llm, memory=memory)
+
+    return chain
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+
+# Chainlit
+@cl.on_chat_start
+async def start():
+    chain = bot()
+    msg = cl.Message(content="Starting...")
+    await msg.send()
+    msg.content = "Hi, I'm MediBot. How can I help you?"
+    await msg.update()
+    cl.user_session.set("chain", chain)
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    chain = cl.user_session.get("chain")
+    cb = cl.AsyncLangchainCallbackHandler()
+    cb.answer_reached = True
+    res = await chain.acall(message.content, callbacks=[cb])
+    await cl.Message(res['text']).send()
